35588d1d1e955aef372bc5351bad23c9ca9f30b7
minasdq <iminasdq@gmail.com>
2020-04-23 10:38:21 +0430

    add index page as home

--- blog/settings.py
+++ blog/settings.py
@@ -120,8 +120,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT=os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfile')
 STATIC_URL = '/static/'
+STATICFILES_DIRS=(
+    os.path.join(BASE_DIR,'static'),
+
+)
 CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
 CKEDITOR_UPLOAD_PATH = "uploads/"
 
